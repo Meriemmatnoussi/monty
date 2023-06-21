46bec332953cb6f89cc887d272e14321b7756dda
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-06-21 15:53:14 -0700

    still trying my best to end this

--- monty.h
+++ monty.h
@@ -12,6 +12,11 @@ typedef struct stack_s
     struct stack_s *next;
 } stack_t;
 
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 /* Function Prototypes */
 void push_stack(stack_t **stack, int value);
 void pall_p(stack_t **stack);
@@ -20,37 +25,3 @@ void free_s(stack_t *stack);
 void process_f(const char *filename, stack_t **stack);
 void pop_s(stack_t **stack);
 void swap_swap(stack_t **stack);
-void add_s(stack_t **stack);
-void nop_g(stack_t **stack);
-
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-#endif /* MONTY_H */
